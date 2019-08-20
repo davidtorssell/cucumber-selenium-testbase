885a5f6cf1001aff685584efe48632701ffffeed
davidtorssell <david.torssell@gmail.com>
2019-08-20 21:00:32 +0200

    updates that allow for grid execution with new cucumber and selenium versions

--- src/test/java/com/diabol/test/WebDriverHelper.java
+++ src/test/java/com/diabol/test/WebDriverHelper.java
@@ -1,17 +1,20 @@
 package com.diabol.test;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.Augmenter;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -103,7 +106,7 @@ public class WebDriverHelper {
     }
 
 
-    private WebDriver remoteDriver(DesiredCapabilities browser) {
+    private WebDriver remoteDriver(Capabilities browser) {
 
         try {
             WebDriver webDriver = new RemoteWebDriver(new URL(gridUrl), browser);
@@ -115,25 +118,26 @@ public class WebDriverHelper {
         }
     }
 
-    private DesiredCapabilities getFirefoxDesiredCapabilities(){
+    private Capabilities getFirefoxDesiredCapabilities(){
 
-        DesiredCapabilities dc = DesiredCapabilities.firefox();
-        dc.setJavascriptEnabled(true);
-        dc.setCapability("marionette", marionette);
+        Capabilities dc = new FirefoxOptions();
+        //      dc.setJavascriptEnabled(true);
 
-        FirefoxProfile profile = new FirefoxProfile();
-        profile.setPreference("intl.accept_languages", firefoxLanguages);
+        //       FirefoxProfile profile = new FirefoxProfile();
+        //     profile.setPreference("intl.accept_languages", firefoxLanguages);
 
         return dc;
     }
 
     private DesiredCapabilities getChromeDesiredCapabilities(){
 
-        DesiredCapabilities dc = DesiredCapabilities.chrome();
-        dc.setJavascriptEnabled(true);
-        dc.setCapability("language", chromeLanguage.toString());
+        DesiredCapabilities cap = new DesiredCapabilities();
+        cap.setBrowserName("chrome");
+        ChromeOptions options = new ChromeOptions();
+        options.merge(cap);
+        //     dc.setCapability("language", chromeLanguage.toString());
 
-        return dc;
+        return cap;
     }
 
     public void saveScreenshot(WebDriver driver, String filename) throws IOException {
